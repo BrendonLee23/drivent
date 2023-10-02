c6373036eee8947acbcf0a04e4ba6a5077f30cb9
Brendo <brendow_prime@hotmail.com>
2023-10-02 04:12:46 -0400

    Feat/ tests Get HotelsById when status code is 404

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -206,4 +206,42 @@ describe('GET /hotels/:hotelId', () => {
 
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
+  describe('when token is valid', () => {
+    it('should respond with status 404 when there is no enrollment for given user', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+
+      const response = await server.get('/hotels/1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 404 when user doesnt have a ticket yet', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createEnrollmentWithAddress(user);
+
+      const response = await server.get('/hotels/1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 404 when there is no hotels', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await prisma.ticketType.create({
+        data: {
+          name: faker.name.findName(),
+          price: faker.datatype.number(),
+          isRemote: false,
+          includesHotel: true,
+        },
+      });
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+
+      const response = await server.get('/hotels/1').set('Authorization', `Bearer ${token}`);
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+  });
 });
