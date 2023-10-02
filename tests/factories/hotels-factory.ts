72e6d4e1a23c7385b137ba946550f40b63dc334a
Brendo <brendow_prime@hotmail.com>
2023-10-02 00:42:30 -0400

    feat/ generate Hotel and Room functions - applied

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -1,11 +1,21 @@
 import { faker } from '@faker-js/faker';
 import { prisma } from '@/config';
 
-export async function createHotel() {
+export async function generateHotel() {
   return await prisma.hotel.create({
     data: {
       name: faker.company.companyName(),
       image: faker.image.imageUrl(),
+    },
+  });
+}
+
+export async function generateRoom(hotelId: number, x: number) {
+  return await prisma.room.create({
+    data: {
+      name: `Room ${x}`,
+      capacity: Number(faker.random.numeric()),
+      hotelId,
     },
   });
 }
