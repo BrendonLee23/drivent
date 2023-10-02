2fa68c86f0d7bf1c424f33ad1618214a100c1eb4
Brendo <brendow_prime@hotmail.com>
2023-10-02 03:12:06 -0400

    Feat/ Get Hotels - when 404 status is returned

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -3,8 +3,10 @@ import supertest from 'supertest';
 import httpStatus from 'http-status';
 import faker from '@faker-js/faker';
 import { cleanDb, generateValidToken } from '../helpers';
-import { createUser } from '../factories';
+import { createEnrollmentWithAddress, createTicket, createUser } from '../factories';
 import app, { init } from '@/app';
+import { prisma } from '@/config';
+import { TicketStatus } from '@prisma/client';
 
 const server = supertest(app);
 
@@ -37,5 +39,42 @@ describe('GET /hotels', () => {
     const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+  describe('when token is valid', () => {
+    it('should respond with status 404 when there is no enrollment for given user', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+
+      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+    it('should respond with status 404 when user doesnt have a ticket yet', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createEnrollmentWithAddress(user);
+
+      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 404 when there is no hotels', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await prisma.ticketType.create({
+        data: {
+          name: faker.name.findName(),
+          price: faker.datatype.number(),
+          isRemote: false,
+          includesHotel: true,
+        },
+      });
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+
+      const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
   });
 });
