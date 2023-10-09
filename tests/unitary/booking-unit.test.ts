ac7a227f156606a99075dd42bb52a258e867c1bb
Brendo <brendow_prime@hotmail.com>
2023-10-09 14:32:02 -0400

    feat/ postBooking unit tests - applied

--- tests/unitary/booking-unit.test.ts
+++ tests/unitary/booking-unit.test.ts
@@ -1,5 +1,5 @@
-import { notFoundError } from '@/errors';
-import { bookingsRepository } from '@/repositories';
+import { forbidenError, notFoundError } from '@/errors';
+import { bookingsRepository, enrollmentRepository, hotelRepository, ticketsRepository } from '@/repositories';
 import { bookingsService } from '@/services';
 
 beforeEach(() => {
@@ -42,6 +42,175 @@ describe('getBooking test', () => {
         createdAt: '2023-10-05T11:30:12.167Z',
         updatedAt: '2023-10-05T11:30:12.167Z',
       },
+    });
+  });
+});
+
+describe('postBooking test', () => {
+  it('should return forbidenError when ticket is remote', async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        TicketType: {
+          isRemote: true,
+          includesHotel: true,
+          status: 'PAID',
+        },
+      };
+    });
+
+    const promise = bookingsService.postBooking(1, 2);
+
+    expect(promise).rejects.toEqual(forbidenError());
+  });
+
+  it("should return forbidenError when ticket doesn't include hotel", async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        status: 'PAID',
+        TicketType: {
+          isRemote: false,
+          includesHotel: false,
+        },
+      };
+    });
+
+    const promise = bookingsService.postBooking(1, 2);
+
+    expect(promise).rejects.toEqual(forbidenError());
+  });
+
+  it('should return forbidenError when ticket is not paid', async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        status: 'RESERVED',
+        TicketType: {
+          isRemote: false,
+          includesHotel: true,
+        },
+      };
+    });
+
+    const promise = bookingsService.postBooking(1, 2);
+
+    expect(promise).rejects.toEqual(forbidenError());
+  });
+
+  it("should return notFounError when didn't find room", async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        status: 'PAID',
+        TicketType: {
+          isRemote: false,
+          includesHotel: true,
+        },
+      };
+    });
+
+    jest.spyOn(hotelRepository, 'findRoomById').mockResolvedValue(null);
+
+    const promise = bookingsService.postBooking(1, 2);
+
+    expect(promise).rejects.toEqual(notFoundError());
+  });
+
+  it('should return forbidenError when room is full', async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        status: 'PAID',
+        TicketType: {
+          isRemote: false,
+          includesHotel: true,
+        },
+      };
+    });
+
+    jest.spyOn(hotelRepository, 'findRoomById').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+        capacity: 0,
+      };
+    });
+
+    jest.spyOn(bookingsRepository, 'getBookingByRoomId').mockImplementationOnce((): any => {
+      return [
+        { id: 1, userId: 1, roomId: 1 },
+        { id: 2, userId: 2, roomId: 1 },
+      ];
+    });
+
+    const promise = bookingsService.postBooking(3, 1);
+
+    expect(promise).rejects.toEqual(forbidenError());
+  });
+
+  it('should return bookingId', async () => {
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockImplementationOnce((): any => {
+      return {
+        status: 'PAID',
+        TicketType: {
+          isRemote: false,
+          includesHotel: true,
+        },
+      };
+    });
+
+    jest.spyOn(hotelRepository, 'findRoomById').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+        capacity: 2,
+      };
+    });
+
+    jest.spyOn(bookingsRepository, 'getBookingByRoomId').mockImplementationOnce((): any => {
+      return [{ id: 1, userId: 1, roomId: 1 }];
+    });
+
+    jest.spyOn(bookingsRepository, 'postBooking').mockImplementationOnce((): any => {
+      return {
+        id: 1,
+      };
+    });
+
+    const booking = await bookingsService.postBooking(2, 1);
+
+    expect(booking).toEqual({
+      bookingId: 1,
     });
   });
 });
